1a49c7c9250620b2b0205a80493458b71859449c
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2018-03-30 00:51:07 +0100

    Working on it. Finally found some DMs :)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log('Using access token', process.env.MASTODON_ACCESS_TOKEN);
 
 // TODO make this dynamic / easy to page through...
 // Replace with a lower max ID to limit to older posts and navigate through
-const MAX_ID = '106888';
+const MAX_ID = '103438';
 
 const Masto = new Mastodon({
 	access_token: process.env.MASTODON_ACCESS_TOKEN,
@@ -25,15 +25,19 @@ function listDMs(err, data, response) {
     for (var i=0; i < data.length; i++) {
 
 	var notification = data[i];
-	if (notification.status && notification.status === 'direct') {
+	if (notification.status && notification.status.visibility === 'direct') {
 
 	    // This is a DM!
 	    
-	    console.log(`Direct message from ${notification.account.username} at ${notification.created_at}:\n${notification.status.content}\n`);
+	    console.log(`Direct Message! ${notification.id} from ${notification.account.username} at ${notification.created_at}:\n${notification.status.content}\n`);
 
 	    foundDMs = true;
 
-	}
+	} else {
+
+	    console.log(`Not a DM :( ${notification.id} ${notification.status.visibility}`);
+
+        }
 
     }
 
